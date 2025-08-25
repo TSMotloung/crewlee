4ddbb399e19e2105b55335743f0ef1b31b7cc98c
TSMotloung <Tsmotlung@gmail.com>
2025-08-26 00:21:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -47,3 +47,17 @@ app.get('/products', async (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Dynamic scraper running on port ${PORT}`));
+
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Crawlee on Railway!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
